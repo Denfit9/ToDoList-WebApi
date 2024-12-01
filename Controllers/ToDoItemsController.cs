ccfd9046f22f051a7f297a1c01ecb1e9d53e6894
Denfit9 <d03062002@gmail.com>
2024-12-01 23:50:11 +0300

    Initial commit
    Just an initial commit with a model and scaffolded controller.

---
+++ Controllers/ToDoItemsController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoItemsController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(Guid id)
+        {
+            var toDoItem = await _context.TodoItems.FindAsync(id);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            return toDoItem;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(Guid id, ToDoItem toDoItem)
+        {
+            if (id != toDoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem toDoItem)
+        {
+            _context.TodoItems.Add(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDoItem", new { id = toDoItem.Id }, toDoItem);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoItem(Guid id)
+        {
+            var toDoItem = await _context.TodoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoItemExists(Guid id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
